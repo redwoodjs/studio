a4f8fcaf18088734eca869b75e7f20e7dc7726aa
David Thyresson <dthyresson@gmail.com>
2024-01-22 15:08:02 -0500

    Inspector wasn't happy with graphql versions. May remove this feature.

--- api/src/services/graphqlInspector/graphqlInspector.ts
+++ api/src/services/graphqlInspector/graphqlInspector.ts
@@ -1,12 +1,42 @@
-import { coverage } from '@graphql-inspector/core'
+import { validate } from '@graphql-inspector/core'
 
 import { SyntaxError } from '@redwoodjs/graphql-server'
 
-// import { makeMergedSchema } from "@redwoodjs/graphql-server"
+import { logger } from 'src/lib/logger'
+import { getSchema } from 'src/util/graphqlSchema'
+
+/**
+ *
+ * @returns import {
+  diff,
+  validate,
+  similar,
+  coverage,
+  Change,
+  InvalidDocument,
+  SimilarMap,
+  SchemaCoverage
+} from '@graphql-inspector/core'
+
+// diff
+const changes: Change[] = diff(schemaA, schemaB)
+// validate
+const invalid: InvalidDocument[] = validate(documentsGlob, schema)
+// similar
+const similar: SimilarMap = similar(schema, typename, threshold)
+// coverage
+const schemaCoverage: SchemaCoverage = coverage(schema, documents)
+// ...
+ */
 
 export const graphqlInspectorCoverage = async () => {
   try {
-    const results = coverage(null, null)
+    const schema = await getSchema()
+    logger.info(JSON.stringify(schema), 'graphqlInspectorCoverage schema')
+
+    const results = validate(schema, [])
+
+    logger.info(JSON.stringify(results), 'graphqlInspectorCoverage schema')
     return results || {}
   } catch (e) {
     throw new SyntaxError(e)
