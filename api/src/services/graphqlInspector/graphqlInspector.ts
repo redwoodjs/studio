4c7112231537c7fa182b92f227ffca8341795e74
David Thyresson <dthyresson@gmail.com>
2024-01-25 09:33:41 -0500

    GraphQL Inspector service no longer errors when coverage is used

--- api/src/services/graphqlInspector/graphqlInspector.ts
+++ api/src/services/graphqlInspector/graphqlInspector.ts
@@ -1,14 +1,11 @@
-import fs from 'node:fs'
+// import fs from 'node:fs'
 
 import { coverage } from '@graphql-inspector/core'
-// /import { CodeFileLoader } from '@graphql-tools/code-file-loader'
-import glob from 'glob'
 
 import { SyntaxError } from '@redwoodjs/graphql-server'
 
 import { logger } from 'src/lib/logger'
 import { getSchema } from 'src/util/graphqlSchema'
-import { getUserProjectPaths } from 'src/util/project'
 
 /**
  * Note: graphql-inspector can perform a number of useful tasks, including:
@@ -20,53 +17,26 @@ import { getUserProjectPaths } from 'src/util/project'
  *
  * Currently, the only one we're using is coverage.
  *
+ * However, coverage is not working as expected but to not loading the document sources
+ * as it expects.
+ *
+ * It return coverage, but isn't applying the source documents to the schema.
+ *
+ * These documents live in the User's Project, not the Studio at the
+ * generateTypeDefGraphQLWeb documents glob location.
+ *
+ * The next step is to figure out how to load the documents for sources,
+ * perhaps using CodeFileLoader for graphql-tools
  *
  * @see: https://github.com/kamilkisiela/graphql-inspector/tree/master/packages/core
  */
-
-// Loads all GraphQL documents from the user's project
-// used to determine schema coverage
-const loadDocumentSources = async () => {
-  const documentsGlob = `${
-    getUserProjectPaths().web.src
-  }/**/!(*.d).{ts,tsx,js,jsx}`
-
-  const documents = []
-  try {
-    logger.info('Loading GraphQL documents')
-    logger.info({ documentsGlob })
-
-    glob(documentsGlob, (err, files) => {
-      if (err) {
-        console.error('Error reading files:', err)
-        return
-      }
-
-      // Read the contents of each matched file
-      files.forEach((file) => {
-        fs.readFile(file, 'utf8', (err, content) => {
-          if (err) {
-            logger.error(`Error reading file ${file}:`, err)
-            return
-          }
-          documents.push(content)
-        })
-      })
-    })
-  } catch (e) {
-    logger.error(e, 'Error loading GraphQL documents')
-  }
-
-  return documents || []
-}
 
 // Returns a JSON object with schema coverage data
 export const graphqlInspectorCoverage = async () => {
   try {
     const schema = await getSchema()
 
-    const sources = await loadDocumentSources()
-    logger.info({ sources }, 'graphqlInspectorCoverage sources')
+    const sources = []
     const results = coverage(schema, sources)
 
     logger.info({ results }, 'graphqlInspectorCoverage results')
