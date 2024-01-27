7c78f80b6264270edb7d97cb075cd8ebef55e483
Tobbe Lundberg <tobbe@tlundberg.com>
2024-01-27 20:20:40 +0100

    revert type change

--- web/src/components/GraphQLSchemaCell/GraphQLSchemaCell.tsx
+++ web/src/components/GraphQLSchemaCell/GraphQLSchemaCell.tsx
@@ -2,9 +2,7 @@ import { useCallback, useEffect } from 'react'
 
 import ReactFlow, {
   Background,
-  Connection,
   Controls,
-  Edge,
   MiniMap,
   addEdge,
   useEdgesState,
@@ -41,7 +39,7 @@ export const Success = ({
   const [edges, setEdges, onEdgesChange] = useEdgesState([])
 
   const onConnect = useCallback(
-    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   )
 
