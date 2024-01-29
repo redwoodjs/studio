4c212fa13cec4d436502c086342ef8257b20a38d
David Thyresson <dthyresson@gmail.com>
2024-01-29 10:26:34 -0500

    Refactor stats into components

--- web/src/components/SQLStatementsCell/SQLStatementsCell.tsx
+++ web/src/components/SQLStatementsCell/SQLStatementsCell.tsx
@@ -3,27 +3,21 @@ import {
   Card,
   Col,
   Grid,
-  LineChart,
   Subtitle,
   Tab,
   TabGroup,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeaderCell,
-  TableRow,
   TabList,
   TabPanel,
   TabPanels,
 } from '@tremor/react'
-import { formatDistanceToNow, parseISO } from 'date-fns'
 import type { SQLStatementsQuery } from 'types/graphql'
 
-import { routes } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
-import { LinkingIcon } from '../LinkingIcon/LinkingIcon'
+import SpansDetailsTable from '../SpansDetailsTable/SpansDetailsTable'
+import StatisticsDetailTable from '../Statistics/StatisticsDetailTable'
+import StatisticsIntervalChart from '../Statistics/StatisticsIntervalChart'
+import StatisticsIntervalTable from '../Statistics/StatisticsIntervalTable'
 
 export const beforeQuery = (props) => {
   return {
@@ -35,7 +29,7 @@ export const beforeQuery = (props) => {
 
 export const QUERY = gql`
   query SQLStatementsQuery {
-    sqlStatements: sqlStatementSpans {
+    details: sqlStatementSpans {
       id
       spanId
       startedAt
@@ -87,20 +81,10 @@ export const Failure = ({ error }: CellFailureProps) => (
 )
 
 export const Success = ({
-  sqlStatements,
+  details,
   statistics,
   statementStats,
 }: CellSuccessProps<SQLStatementsQuery>) => {
-  const data = statistics.map((item) => {
-    return {
-      date: item.intervalStartedAt,
-      count: item.statisticCount,
-      min: item.minDurationMs,
-      max: item.maxDurationMs,
-      avg: item.avgDurationMs,
-    }
-  })
-
   return (
     <TabGroup>
       <TabList className="mt-8">
@@ -111,165 +95,26 @@ export const Success = ({
         <TabPanel>
           <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
             <Col numColSpanSm={2} numColSpanLg={3}>
-              <Card>
-                <Subtitle>Overall vs Time (5 Min Intervals)</Subtitle>
-                <LineChart
-                  index={'date'}
-                  data={data}
-                  categories={['min', 'max', 'avg']}
-                  colors={['orange', 'emerald', 'rose']}
-                />
-              </Card>
+              <StatisticsIntervalChart
+                statistics={statistics}
+                interval={'5 Min'}
+              />
             </Col>
             <Col numColSpanSm={2} numColSpanLg={3}>
-              <Card>
-                <Subtitle>Stats Overall</Subtitle>
-                <Table className="mt-5">
-                  <TableHead>
-                    <TableRow>
-                      <TableHeaderCell className="max-w-96">
-                        SQL Statement
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Count
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Min Duration (msec)
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Max Duration (msec)
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Avg. Duration (msec)
-                      </TableHeaderCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {statementStats.map((item) => {
-                      return (
-                        <TableRow key={item.attributeValue}>
-                          <TableCell className="!text-wrap max-w-96 !whitespace-normal !break-all">
-                            {item.attributeValue}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.statisticCount}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.minDurationMs}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.maxDurationMs}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.avgDurationMs}
-                          </TableCell>
-                        </TableRow>
-                      )
-                    })}
-                  </TableBody>
-                </Table>
-              </Card>
+              <StatisticsDetailTable statistics={statementStats} />
             </Col>
             <Col numColSpanSm={2} numColSpanLg={3}>
-              <Card>
-                <Subtitle>Overall Stats (5 Min Intervals)</Subtitle>
-                <Table className="mt-5">
-                  <TableHead>
-                    <TableRow>
-                      <TableHeaderCell>Ago</TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Count
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Min Duration (msec)
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Max Duration (msec)
-                      </TableHeaderCell>
-                      <TableHeaderCell className="text-right">
-                        Avg. Duration (msec)
-                      </TableHeaderCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {data.map((item) => {
-                      return (
-                        <TableRow key={item.date}>
-                          <TableCell>
-                            {formatDistanceToNow(parseISO(item.date), {
-                              includeSeconds: true,
-                              addSuffix: true,
-                            })}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.count}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.min}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.max}
-                          </TableCell>
-                          <TableCell className="text-right">
-                            {item.avg}
-                          </TableCell>
-                        </TableRow>
-                      )
-                    })}
-                  </TableBody>
-                </Table>
-              </Card>
+              <StatisticsIntervalTable
+                statistics={statistics}
+                interval={'5 Min'}
+              />
             </Col>
           </Grid>
         </TabPanel>
         <TabPanel>
           <Card>
             <Subtitle>Recent SQL Statements</Subtitle>
-            <Table className="mt-5">
-              <TableHead>
-                <TableRow>
-                  <TableHeaderCell></TableHeaderCell>
-                  <TableHeaderCell className="max-w-96">
-                    SQL Statement
-                  </TableHeaderCell>
-                  <TableHeaderCell>Ago</TableHeaderCell>
-                  <TableHeaderCell className="text-right">
-                    Duration (msec)
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-right">
-                    Duration (s)
-                  </TableHeaderCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {sqlStatements.map((item) => {
-                  return (
-                    <TableRow key={item.id}>
-                      <TableCell>
-                        <LinkingIcon
-                          to={routes.opentelemetrySpan({ id: item.spanId })}
-                        />
-                      </TableCell>
-                      <TableCell className="!text-wrap max-w-96 !whitespace-normal !break-all">
-                        {item.attributeValue}
-                      </TableCell>
-                      <TableCell>
-                        {formatDistanceToNow(parseISO(item.startedAt), {
-                          includeSeconds: true,
-                          addSuffix: true,
-                        })}
-                      </TableCell>
-                      <TableCell className="text-right">
-                        {item.durationMs}
-                      </TableCell>
-                      <TableCell className="text-right">
-                        {item.durationSec}
-                      </TableCell>
-                    </TableRow>
-                  )
-                })}
-              </TableBody>
-            </Table>
+            <SpansDetailsTable details={details} caption="SQL" />
           </Card>
         </TabPanel>
       </TabPanels>
