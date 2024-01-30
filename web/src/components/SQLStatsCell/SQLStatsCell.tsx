1b9e0075c41271400f7d7fba4d4deadda0cf1293
David Thyresson <dthyresson@gmail.com>
2024-01-30 08:33:53 -0500

    add sql and gql stats to dashboard

---
+++ web/src/components/SQLStatsCell/SQLStatsCell.tsx
@@ -0,0 +1,76 @@
+import { Text, BarList, Metric, Card, Flex } from '@tremor/react'
+import type {
+  SQLDashboardStatsQuery,
+  SQLDashboardStatsQueryVariables,
+} from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+import ChartHeading from 'src/components/Charts/ChartHeading'
+import { DatabaseIcon } from 'src/icons/Icons'
+
+export const QUERY = gql`
+  query SQLDashboardStatsQuery {
+    stats: sqlStatementAttributeStatistics {
+      attributeValue
+      statisticCount
+      minDuration
+      minDurationMs
+      minDurationSec
+      maxDuration
+      maxDurationMs
+      maxDurationSec
+      avgDuration
+      avgDurationMs
+      avgDurationSec
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<SQLDashboardStatsQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  stats,
+}: CellSuccessProps<
+  SQLDashboardStatsQuery,
+  SQLDashboardStatsQueryVariables
+>) => {
+  const data = stats.map((stat) => {
+    return {
+      name: stat.attributeValue,
+      value: stat.statisticCount,
+    }
+  })
+  const total = data.reduce((acc, curr) => acc + curr.value, 0)
+
+  return (
+    <Card>
+      <ChartHeading
+        caption="SQL Stats"
+        icon={DatabaseIcon}
+        tooltip="SQL Stats"
+      />
+      <Flex
+        justifyContent="start"
+        alignItems="baseline"
+        className="mt-6 space-x-2"
+      >
+        <Metric>{total}</Metric>
+        <Text>SQL Statements Made</Text>
+      </Flex>
+      <Flex className="mt-6">
+        <Text>SQL Statement</Text>
+        <Text className="text-right">Count</Text>
+      </Flex>
+      <BarList data={data} className="mt-2" />
+    </Card>
+  )
+}
