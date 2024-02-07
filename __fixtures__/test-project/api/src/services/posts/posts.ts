b3d2e277e662dd75d5e3bc04332bcca25f6ecfcf
David Thyresson <dthyresson@gmail.com>
2024-02-07 10:40:52 -0500

    Demo n+1 author, include

--- __fixtures__/test-project/api/src/services/posts/posts.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.ts
@@ -7,12 +7,14 @@ import type {
 import { db } from 'src/lib/db'
 
 export const posts: QueryResolvers['posts'] = () => {
+  // { include: { author: true } }
   return db.post.findMany()
 }
 
 export const post: QueryResolvers['post'] = ({ id }) => {
   return db.post.findUnique({
     where: { id },
+    // include: { author: true },
   })
 }
 
@@ -37,6 +39,10 @@ export const deletePost: MutationResolvers['deletePost'] = ({ id }) => {
 
 export const Post: PostRelationResolvers = {
   author: (_obj, { root }) => {
+    // if (root.author) {
+    //   return root.author
+    // }
+
     return db.post.findUnique({ where: { id: root?.id } }).author()
   },
 }
