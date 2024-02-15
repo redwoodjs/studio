17141168b5a1b5a7f473afa5cde1ba262b937a84
David Thyresson <dthyresson@gmail.com>
2024-02-15 09:55:07 -0500

    If SSR is disabled, don't preview

--- api/src/services/ogTagPreview/ogTagPreview.ts
+++ api/src/services/ogTagPreview/ogTagPreview.ts
@@ -1,13 +1,29 @@
 import ogs from 'open-graph-scraper'
 import type { QueryResolvers } from 'types/graphql'
 
+import { getUserProjectConfig } from 'src/util/project'
+
 export const ogTagPreview: QueryResolvers['ogTagPreview'] = async ({
   url,
   customUserAgent,
 }) => {
+  const config = await getUserProjectConfig()
+
   // Use an example of Google's user agent if none is provided
   customUserAgent ??=
     'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36'
+
+  // short-cut in the case that SSR is disabled
+  if (!config.experimental?.streamingSsr?.enabled) {
+    return {
+      id: url,
+      userAgent: customUserAgent,
+      error: true,
+      result: {
+        message: 'No OG Tag Preview possible because SSR is disabled.',
+      },
+    }
+  }
 
   const html = await (
     await fetch(url, {
