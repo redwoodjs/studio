d76a41aa571c4a17d6b995e247a4d45a0ef71d36
David Thyresson <dthyresson@gmail.com>
2024-02-29 18:46:02 -0500

    Rework connection watcher to use GraphQL Health Check

--- api/src/util/connectionWatching.ts
+++ api/src/util/connectionWatching.ts
@@ -2,28 +2,39 @@ import { liveQueryStore } from '@redwoodjs/realtime'
 
 import { db } from 'src/lib/db'
 
-const STATUS_CHECK_INTERVAL_MS = 2000
+import { getUserProjectConfig } from './project'
+
+const STATUS_CHECK_INTERVAL_MS = 17_000
 let statusInterval: NodeJS.Timeout | null = null
 
 export function startConnectionWatching() {
+  const webConfig = getUserProjectConfig().web
+  const webHost = webConfig.host ?? 'localhost'
+  const graphqlEndpoint =
+    webConfig.apiGraphQLUrl ??
+    `http://${webHost}:${webConfig.port}${webConfig.apiUrl}/graphql`
+
   if (statusInterval) {
     stopConnectionWatching()
   }
 
   statusInterval = setInterval(async () => {
-    // Ping the web server to see if it's running
-    let webRespondedOk = false
+    // Ping the user project's graphQL health check endpoint
+    // to see if it's running
+    let isConnected = false
     try {
-      webRespondedOk = (await fetch('http://localhost:8910/')).ok
+      isConnected = (await fetch(`${graphqlEndpoint}/health`)).ok
     } catch (_error) {
-      //
+      // If the web server is not running, the fetch will throw an error
+      // but eat the error and webRespondedOk will be false
+      console.warn('🚨 Development Server not connected.')
     }
 
     // Upsert with a hard-coded ID of 1 as we only ever want the one row
     await db.connectionStatus.upsert({
       where: { id: 1 },
-      create: { id: 1, developmentServer: webRespondedOk },
-      update: { developmentServer: webRespondedOk },
+      create: { id: 1, developmentServer: isConnected },
+      update: { developmentServer: isConnected },
     })
 
     // Invalidate the live query
