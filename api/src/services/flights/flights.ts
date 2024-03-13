305b37e038029322bf12eedffc79e1466c5e053d
David Thyresson <dthyresson@gmail.com>
2024-03-13 18:02:29 +0100

    update preview

--- api/src/services/flights/flights.ts
+++ api/src/services/flights/flights.ts
@@ -1,3 +1,4 @@
+import { logger } from '@prisma/internals'
 import type { QueryResolvers } from 'types/graphql'
 
 import { ValidationError } from '@redwoodjs/graphql-server'
@@ -8,12 +9,16 @@ import type { Flight } from '../../../db/client'
 
 const decodeFlightMetadata = (flight: Flight) => {
   if (flight.encoding === 'base64') {
-    const metadata = JSON.parse(
-      Buffer.from(flight.encodedMetadata, 'base64').toString('utf-8')
-    )
+    try {
+      const metadata = JSON.parse(
+        Buffer.from(flight.encodedMetadata, 'base64').toString('utf-8')
+      )
 
-    console.log(metadata)
-    return metadata
+      return metadata
+    } catch (e) {
+      logger.error(e)
+      throw new ValidationError(`Invalid metadata for flight ${flight.id}`)
+    }
   }
 
   throw new ValidationError(`Unsupported encoding type ${flight.encoding}`)
@@ -21,31 +26,48 @@ const decodeFlightMetadata = (flight: Flight) => {
 
 const decodeFlightPayload = (flight: Flight) => {
   if (flight.encoding === 'base64') {
-    return Buffer.from(flight.encodedPayload, 'base64').toString('utf-8')
+    try {
+      return Buffer.from(flight.encodedPayload, 'base64').toString('utf-8')
+    } catch (e) {
+      logger.error(e)
+      throw new ValidationError(`Invalid payload for flight ${flight.id}`)
+    }
   }
 
   throw new ValidationError(`Unsupported encoding type ${flight.encoding}`)
 }
 
 const decodeFlightPerformance = (flight: Flight) => {
-  const payload = decodeFlightPayload(flight)
-  const metadata = decodeFlightMetadata(flight)
-  const performance = {
-    startedAt: new Date(metadata?.['performance'].startedAt).toISOString(),
-    endedAt: new Date(metadata?.['performance'].endedAt).toISOString(),
-    duration: metadata?.['performance'].duration || 0,
-    sizeInBytes: Buffer.from(payload).length,
-  }
+  try {
+    const payload = decodeFlightPayload(flight)
+    const metadata = decodeFlightMetadata(flight)
+    const performanceMeta = metadata?.['performance'] || {}
+    const performance = {
+      startedAt: new Date(performanceMeta.startedAt).toISOString(),
+      endedAt: new Date(performanceMeta.endedAt).toISOString(),
+      duration: performanceMeta.duration || 0,
+      sizeInBytes: Buffer.from(payload).length,
+    }
 
-  return performance
+    return performance
+  } catch (e) {
+    logger.error(e)
+    return {
+      startedAt: new Date().toISOString(),
+      endedAt: new Date().toISOString(),
+      duration: 0,
+      sizeInBytes: 0,
+    }
+  }
 }
 
 const previewFlightPayload = (flight: Flight) => {
   const metadata = decodeFlightMetadata(flight)
+  const performance = decodeFlightPerformance(flight)
   const url = metadata?.['request']?.['url']
   const id = metadata?.['rsc']?.['rscId'] || metadata?.['rsc']?.['rsfId']
-  const duration = metadata?.['performance']?.['duration'] || 0
-  return `${id}#${url}-${duration}ms`
+
+  return `${id}#${url} at ${performance.startedAt} for (${performance.duration}ms)`
 }
 
 export const flights: QueryResolvers['flights'] = async () => {
