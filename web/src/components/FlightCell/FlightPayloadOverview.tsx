139b96a60a221593af3cb767d8bb11a24c7f17fc
David Thyresson <dthyresson@gmail.com>
2024-03-17 13:10:46 +0100

    more tweeeeks

--- web/src/components/FlightCell/FlightPayloadOverview.tsx
+++ web/src/components/FlightCell/FlightPayloadOverview.tsx
@@ -1,7 +1,8 @@
 import { Card, Flex, Icon } from '@tremor/react'
+import { parseISO, formatISO9075 } from 'date-fns'
 import type { FlightPreview } from 'types/graphql'
 
-import { FlightPayloadIcon } from 'src/icons/Icons'
+import { FlightPayloadIcon, InfoIcon } from 'src/icons/Icons'
 
 const Heading = ({ preview }) => {
   const metadata = preview.flight.metadata
@@ -35,22 +36,57 @@ const Payload = ({ preview }) => {
   )
 }
 
+const Duration = ({ performance }) => {
+  return <>{performance?.duration.toFixed(3) || 0} ms</>
+}
+
+const RequestedAt = ({ performance }) => {
+  return (
+    <>
+      {formatISO9075(parseISO(performance?.startedAt), {
+        format: 'extended',
+        representation: 'complete',
+      })}
+    </>
+  )
+}
+
+const Size = ({ performance }) => {
+  return <>{performance?.sizeInBytes || 0} bytes</>
+}
+
 const PerformanceDetails = ({ preview }) => {
   const performance = preview.flight.performance
 
   return (
-    <>
-      <h4 className="font-small mt-4 text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
-        Flight render duration: {performance?.duration.toFixed(3) || 0} ms
+    <div className="border-1 mt-4 rounded-md bg-tremor-background-muted p-4 ring-1 ring-inset ring-tremor-ring dark:bg-dark-tremor-background-subtle dark:ring-dark-tremor-ring">
+      <h4 className="font-small mt-2 text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
+        <span className="font-semibold">Render duration:</span>
+        <span className="pl-2">
+          <Duration performance={performance} />
+        </span>
       </h4>
-      <h4 className="font-small mt-4 text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
-        Flight requested at: {performance?.startedAt}
+      <h4 className="font-small mt-2 text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
+        <span className="font-semibold">Requested at:</span>
+        <span className="pl-2">
+          <RequestedAt performance={performance} />
+        </span>
       </h4>
-      <h4 className="font-small mt-4 text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
-        Flight payload size:{' '}
-        {((performance?.sizeInBytes || 0) / 1_024.0).toFixed(3)} kb
+      <h4 className="font-small text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong">
+        <div className="flex items-center justify-start space-x-1">
+          <span className="font-semibold">Payload size:</span>
+          <span className="pl-1">
+            <Size performance={performance} />
+          </span>
+          <Icon
+            icon={InfoIcon}
+            size="sm"
+            variant="simple"
+            tooltip="Note: Size of the flight payload includes extra bytes for ids and other formatting characters per chunk."
+          />
+        </div>
       </h4>
-    </>
+    </div>
   )
 }
 
