04c6dc7994015ccc6b9ecfa8760774c52f357391
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-24 20:58:02 +0200

    Make navigation groups toggleable

---
+++ web/src/layouts/SidebarLayout/navigationStructure.ts
@@ -0,0 +1,160 @@
+import { useCallback, useState } from 'react'
+
+import type { AvailableRoutes } from '@redwoodjs/router'
+
+import {
+  AboutIcon,
+  DashboardIcon,
+  ErdIcon,
+  GraphQLIcon,
+  InboxIcon,
+  SettingsIcon,
+  OGTagPreviewIcon,
+  OperationsIcon,
+  PlaygroundIcon,
+  SpansIcon,
+  SqlStatementsIcon,
+  TemplatesIcon,
+  TracesIcon,
+} from 'src/icons/Icons'
+
+import type { TNavigationItem } from './NavigationItem'
+
+interface NavigationGroup {
+  id: string
+  title: string
+  items: TNavigationItem[]
+  closed: boolean
+}
+
+export function useNavigationStructure(routes: AvailableRoutes) {
+  const [closedGroups, setClosedGroups] = useState(new Set<string>())
+
+  const toggleGroup = useCallback(
+    (groupId: string) => {
+      if (!closedGroups.delete(groupId)) {
+        closedGroups.add(groupId)
+      }
+
+      setClosedGroups(new Set(closedGroups))
+    },
+    [closedGroups]
+  )
+
+  const dashboardNavigation: TNavigationItem[] = [
+    { name: 'Dashboard', to: routes.home(), icon: DashboardIcon },
+  ]
+
+  const telemetryNavigation: TNavigationItem[] = [
+    {
+      name: 'Traces',
+      to: routes.opentelemetryTraces(),
+      icon: TracesIcon,
+    },
+    {
+      name: 'Spans',
+      to: routes.opentelemetrySpans(),
+      icon: SpansIcon,
+    },
+  ]
+
+  const databaseNavigation: TNavigationItem[] = [
+    {
+      name: 'Schema',
+      to: routes.databaseSchema(),
+      icon: ErdIcon,
+    },
+    {
+      name: 'SQL Statements',
+      to: routes.databaseSqlStatements(),
+      icon: SqlStatementsIcon,
+    },
+  ]
+
+  const graphQLNavigation: TNavigationItem[] = [
+    {
+      name: 'Schema',
+      to: routes.graphqlSchema(),
+      icon: GraphQLIcon,
+    },
+    {
+      name: 'Operations',
+      to: routes.graphQLOperations(),
+      icon: OperationsIcon,
+    },
+    {
+      name: 'Playground',
+      to: routes.graphiql(),
+      icon: PlaygroundIcon,
+    },
+  ]
+
+  const mailerNavigation: TNavigationItem[] = [
+    { name: 'Inbox', to: routes.mailerInbox(), icon: InboxIcon },
+    {
+      name: 'Templates',
+      to: routes.mailerTemplatePreview(),
+      icon: TemplatesIcon,
+    },
+  ]
+
+  const ssrNavigation: TNavigationItem[] = [
+    {
+      name: 'OG Tag Preview',
+      to: routes.ogTagPreview(),
+      icon: OGTagPreviewIcon,
+    },
+  ]
+
+  const studioNavigation: TNavigationItem[] = [
+    { name: 'Settings', to: routes.settings(), icon: SettingsIcon },
+    { name: 'About', to: routes.about(), icon: AboutIcon },
+  ]
+
+  const navigationGroups: Array<NavigationGroup> = [
+    {
+      id: 'dashboard',
+      title: '',
+      items: dashboardNavigation,
+      closed: closedGroups.has('dashboard'),
+    },
+    {
+      id: 'monitoring',
+      title: 'Monitoring',
+      items: telemetryNavigation,
+      closed: closedGroups.has('monitoring'),
+    },
+    {
+      id: 'graphql',
+      title: 'GraphQL',
+      items: graphQLNavigation,
+      closed: closedGroups.has('graphql'),
+    },
+    {
+      id: 'database',
+      title: 'Database',
+      items: databaseNavigation,
+      closed: closedGroups.has('database'),
+    },
+    {
+      id: 'mailer',
+      title: 'Mailer',
+      items: mailerNavigation,
+      closed: closedGroups.has('mailer'),
+    },
+    {
+      id: 'ssr',
+      title: 'SSR',
+      items: ssrNavigation,
+      closed: closedGroups.has('ssr'),
+    },
+    {
+      id: 'studio',
+      title: 'Studio',
+      items: studioNavigation,
+      closed: closedGroups.has('studio'),
+    },
+  ]
+
+  return { navigationStructure: navigationGroups, toggleGroup }
+}
