0692ff5e154c34d7f4c99690cec2a9087f28f024
Tobbe Lundberg <tobbe@tlundberg.com>
2024-10-21 13:23:15 +0200

    fix(rsc): Rename Flight to RSC Payload in a few places

--- web/src/pages/RscPayloadsPage/RscPayloadsPage.tsx
+++ web/src/pages/RscPayloadsPage/RscPayloadsPage.tsx
@@ -5,19 +5,19 @@ import { Metadata } from '@redwoodjs/web'
 
 import FlightsCell from 'src/components/FlightsCell'
 
-const FlightsPage = () => {
+const RscPayloadsPage = () => {
   return (
     <>
       <Metadata
-        title="Flight Previews"
-        description="Track RSC Flight Payload status and view details."
+        title="RSC Payloads"
+        description="Track RSC Payload status and view details."
       />
       <div className="mb-4 flex items-center space-x-2">
         <Link
           to={routes.flights()}
           className="text-tremor-link hover:text-tremor-brand"
         >
-          <Title>Flight Previews</Title>
+          <Title>RSC Payloads</Title>
         </Link>
       </div>
 
@@ -26,4 +26,4 @@ const FlightsPage = () => {
   )
 }
 
-export default FlightsPage
+export default RscPayloadsPage
