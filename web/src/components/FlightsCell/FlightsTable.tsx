eb945f2979b4aa3ac0c8013653b5a5401ab63564
Tobbe Lundberg <tobbe@tlundberg.com>
2024-10-23 23:37:10 +0200

    fix(flights): Clean up flight title

--- web/src/components/FlightsCell/FlightsTable.tsx
+++ web/src/components/FlightsCell/FlightsTable.tsx
@@ -16,7 +16,7 @@ import type { RscChunkMessage } from 'src/components/RscParser/types'
 
 interface RscChunkMessageDataExtended {
   id: string
-  preview: string
+  title: string
   fetchUrl: string
   fetchHeaders: Record<string, string | undefined>
   fetchStartTime: number
@@ -33,16 +33,30 @@ interface RscChunkMessageExtended extends RscChunkMessage {
 
 export const FlightsTable = ({ flightsPreview }: FlightsPreviewQuery) => {
   const messages = flightsPreview.flights.map((flight) => {
-    const { preview, payload, metadata, performance } = flight
+    const { id, preview, payload, metadata, performance } = flight
+
+    // Assume it's a server action if `preview` contains a `#`
+    const rsaTitle = preview.includes('#') ? preview.split('/').at(-1) : ''
+
+    // metadata.request.url will be something like this for RSCs:
+    // /__rwjs__Routes?__rwjs__pathname=/blog/hello-world&__rwjs__search=?foo=bar&baz=qux
+    const fetchUrl = metadata?.request?.url || ''
+    const splitUrl = fetchUrl.split('?').slice(1).join('?').split('&')
+    const urlLocation = splitUrl[0].replace('__rwjs__pathname=', '')
+    const urlSearch = splitUrl
+      .slice(1)
+      .join('&')
+      .replace(/^__rwjs__search=/, '')
+    const rscTitle = urlLocation + urlSearch
 
     return {
       type: 'RSC_CHUNK',
       tabId: 0,
       data: {
-        id: flight.id,
-        preview: preview,
-        fetchUrl: metadata?.['request']?.['url'] || '',
-        fetchHeaders: metadata?.['request']?.['headers'] || {},
+        id,
+        title: rsaTitle || rscTitle,
+        fetchUrl: metadata?.request?.url || '',
+        fetchHeaders: metadata?.request?.headers || {},
         fetchStartTime: 0,
         chunkStartTime: parseISO(performance.startedAt).getTime(),
         chunkEndTime: parseISO(performance.endedAt).getTime(),
@@ -72,7 +86,7 @@ export const FlightsTable = ({ flightsPreview }: FlightsPreviewQuery) => {
             .slice()
             .reverse()
             .map((message, idx) => {
-              const data = message?.['data']
+              const data = message?.data
 
               if (data === undefined) {
                 return
@@ -85,7 +99,7 @@ export const FlightsTable = ({ flightsPreview }: FlightsPreviewQuery) => {
                       to={routes.flight({ id: data.id })}
                       className="font-semibold text-tremor-content hover:text-tremor-brand dark:text-dark-tremor-content-emphasis dark:hover:text-dark-tremor-brand"
                     >
-                      {data.preview} - {data.fetchUrl}
+                      {data.title}
                     </Link>
                   </TableCell>
                   <TableCell className="text-right">
